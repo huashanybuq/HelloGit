36c732ff4fa75e0aa2e4fa616c244207d002bcd3
hr_glb <hr_glb@126.com>
2020-05-28 10:11:22 +0800

    first commit

---
+++ HelloGit/src/main/java/org/huangrui/git/HelloGit.java
@@ -0,0 +1,11 @@
+package org.huangrui.git;
+
+/**
+ * @author hr
+ * @create 2020-05-28 10:03
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
